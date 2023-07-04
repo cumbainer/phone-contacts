22b8ac1c74b91918a8ae678b4a00a8112bc85316
cumbainer <shtaiier228666@gmail.com>
2023-07-04 18:25:56 +0300

    Init project

---
+++ src/main/java/ua/shtaiier/phonecontacts/PhoneContactsApplication.java
@@ -0,0 +1,13 @@
+package ua.shtaiier.phonecontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneContactsApplication.class, args);
+    }
+
+}
